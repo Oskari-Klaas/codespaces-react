63c05a2285b754a5a6a4d21bc329c74628215540
Oskari-Klaas <oskari.klaas@voco.ee>
2026-09-08 12:09:44 +0000

    Fixes*

--- src/components/ExpenseDate.jsx
+++ src/components/ExpenseDate.jsx
@@ -8,10 +8,10 @@ const ExpenseDate = (props) => {
   const year = dateObj.getFullYear();
 
   return (
-    <div>
-      <div>{month}</div>
-      <div>{day}</div>
-      <div>{year}</div>
+    <div className='expense-date'>
+      <div className='expense-date__month'>{month}</div>
+      <div className='expense-date__day'>{day}</div>
+      <div className='expense-date__year'>{year}</div>
     </div>
   );
 };
