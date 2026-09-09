436cf15c0d6a7aa81538ef8f7421b5c8be2b6e76
Oskari-Klaas <oskari.klaas@voco.ee>
2026-09-09 08:34:04 +0000

    first commit bs idk ül 2 tehtud fucking plaese

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,11 @@
 import './App.css';
-import ExpensiveItem from './components/ExpensiveItem';
-import Expenses from './components/Expenses';
+import ExpenseItem from './components/ExpenseItem';
 
 const App = () => {
-  const expenses = [
-  {
-    date: new Date(2024, 10, 12),
-    title: 'New book',
-    price: 30.99
-  },
-  {
-    date: new Date(2024, 10, 12),
-    title: 'New jeans',
-    price: 99.99
-  }
-]
-
-
   return (
     <div className="App">
-      <Expenses data={expenses}/>
+      <ExpenseItem />
+      <ExpenseItem />
     </div>
   );
 }
