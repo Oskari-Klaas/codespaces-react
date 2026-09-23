5e7d79cb1d6d65c4059769bd84f6a53ac67c8387
Oskari-Klaas <oskari.klaas@voco.ee>
2026-09-23 06:53:48 +0000

    Ül 4 Andmete suunamine (bottom-up) ja väärtuse hetkeseisu lugemine ning määramine

--- src/components/Expenses.jsx
+++ src/components/Expenses.jsx
@@ -1,12 +1,25 @@
+import React, { useState } from 'react'
 import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
 import './Expenses.css';
+import ExpensesFilter from '../expenses/ExpensesFilter';
 
 const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState('2023');
 
+  console.log(filteredYear)
+
+  const filterChangeHandler = (selectedYear) => {
+    console.log(SelectedYear);
+    setFilteredYear(selectedYear);
+  };
 
   return (
     <Card className="expenses">
+      <ExpensesFilter
+      selected={filteredYear}
+      onChangeFilter={filterChangeHandler}
+      />
       <ExpenseItem data={props.items[0]}/>
       <ExpenseItem data={props.items[1]}/>
     </Card>
